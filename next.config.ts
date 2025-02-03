915e9f4c22b7c3cf045112858ff825f41b16a6f3
Syed Ali <syedyusufali6626@gmail.com>
2025-02-03 20:45:49 +0530

    fixed build error

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
 
+const nextConfig: NextConfig = {
 
-const withNextIntl = createNextIntlPlugin('./src/app/i18n.ts');
+};
 
-const nextConfig: NextConfig = {};
-
-export default withNextIntl(nextConfig);
+export default nextConfig;
